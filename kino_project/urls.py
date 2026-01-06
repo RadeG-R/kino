538c868a8b5ad141104e51007f03b177946bad54
RadeG-R <radek.rychel123@gmail.com>
2026-01-06 16:02:11 +0100

    Update urls.py

--- kino_project/urls.py
+++ kino_project/urls.py
@@ -1,16 +1,15 @@
-# kino_project/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # Przenosimy WSZYSTKIE ścieżki do main/urls.py
+    path('admin/', admin.site.urls),  # poprawione: bez linków
+    # Wszystkie główne ścieżki w main.urls
     path('', include('main.urls')),
 ]
 
+# Obsługa static i media tylko w DEBUG (na Cloud Run DEBUG=True)
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
